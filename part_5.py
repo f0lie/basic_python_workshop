51f326e57f5b01522de0aff0aeb254ddd9200780
Alex Diep <alexdiep2501@gmail.com>
2016-10-17 19:28:37 -0400

    add part 5

---
+++ part_5.py
@@ -0,0 +1,21 @@
+# Tuples are immutable
+# You cannot change them once you define them
+# Meaning that if pos is a tuple, we can't update the pos
+
+# But we can fix that with lists
+pos = [0,0]
+velo = [1,1]
+
+print(pos, velo)
+
+# Much like tuples, we get numbers by indexing
+print(pos[0])
+print(pos[1])
+
+# Now we can modify pos as a pair of numbers
+for i in range(10):
+    print(pos)
+    pos[0] += velo[0]
+    pos[1] += velo[1]
+
+print(pos)
